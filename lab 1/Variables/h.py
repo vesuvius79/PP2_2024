bfd3c08f0c62ef746df1f29a08b7d2fb8f4111ee
Saken <orazgaliyev.saken@gmail.com>
2024-02-09 21:34:02 +0600

    lab3

--- lab 1/Variables/h.py
+++ lab 1/Variables/h.py
@@ -1,4 +1,15 @@
-i = int(input())
-while(i >= 0):
-    print("meow")
-    
+# 1 answer
+class Ali():
+    def __init__(self):
+        pass
+    def getString(self):
+        self.a = input("Enter the string: ")
+        pass
+    def printString(self):
+        print(self.a.upper())
+        pass
+   
+ex1 = Ali()     
+ex1.getString()
+ex1.printString()
+print("===================")
